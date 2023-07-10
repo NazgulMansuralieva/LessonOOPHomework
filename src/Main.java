cd3d36967b1bbbfaf63ced2743e8ca355809963c
NazgulMansuralieva <nazyu1994@gmail.com>
2023-07-10 14:41:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+    Uchburchtuk uchburchtuk=new Uchburchtuk();
+    uchburchtuk.a=4.5;
+    uchburchtuk.b=4.3;
+    uchburchtuk.c=4.8;
+    uchburchtuk.area(uchburchtuk.a, uchburchtuk.b, uchburchtuk.c);
+    }
+}
---
+++ src/Uchburchtuk.java
@@ -0,0 +1,11 @@
+public class Uchburchtuk {
+    double a;
+    double b;
+    double c;
+    public void area(double a,double b,double c){
+        double p=(a+b+c)/2;
+        System.out.println("Area:"+Math.sqrt(p*(p-a)*(p-b)*(p-c)));
+
+    }
+
+}
